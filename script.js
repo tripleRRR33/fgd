d90ccd2d745a1d261404da4ec0de7c3b3f028ad0
tripleRRR33 <florianroy555@gmail.com>
2025-01-07 10:49:32 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+function saveCharacterData() {
+    const characterData = {
+        name: document.getElementById('name').value,
+        age: document.getElementById('age').value,
+        gender: document.getElementById('gender').value,
+        appearance: document.getElementById('appearance').value,
+        profession: document.getElementById('profession').value,
+        nickname: document.getElementById('nickname').value,
+        traits: document.getElementById('traits').value,
+        strengths: document.getElementById('strengths').value,
+        goals: document.getElementById('goals').value,
+        fears: document.getElementById('fears').value,
+        values: document.getElementById('values').value,
+        past: document.getElementById('past').value,
+        keyEvents: document.getElementById('key-events').value,
+        conflicts: document.getElementById('conflicts').value,
+        relations: document.getElementById('relations').value,
+        dynamics: document.getElementById('dynamics').value,
+        loveRelations: document.getElementById('love-relations').value,
+        evolution: document.getElementById('evolution').value,
+        longTermGoals: document.getElementById('long-term-goals').value,
+        changingEvents: document.getElementById('changing-events').value,
+        quote: document.getElementById('quote').value,
+        distinctiveAppearance: document.getElementById('distinctive-appearance').value,
+        hobbies: document.getElementById('hobbies').value,
+        memories: document.getElementById('memories').value
+    };
+    localStorage.setItem('characterData', JSON.stringify(characterData));
+    alert('Données enregistrées!');
+}
+
+function downloadPDF() {
+    html2canvas(document.getElementById('character-sheet'), {
+        onrendered: function(canvas) {
+            var imgData = canvas.toDataURL('image/png');
+            var doc = new jsPDF();
+            doc.addImage(imgData, 'PNG', 10, 10);
+            doc.save('fiche_personnage.pdf');
+        }
+    });
+}
+
+window.onload = function() {
+    if (localStorage.getItem('characterData')) {
+        const characterData = JSON.parse(localStorage.getItem('characterData'));
+        for (const key in characterData) {
+            if (characterData.hasOwnProperty(key)) {
+                document.getElementById(key).value = characterData[key];
+            }
+        }
+    }
+}
